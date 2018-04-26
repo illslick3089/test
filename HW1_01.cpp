347c5acd25fec202a2701c95792c71d4241796e3
Supattanachai <illsilck3089@gmail.com>
2018-04-26 18:26:28 +0700

    test2

---
+++ HW1_01.cpp
@@ -0,0 +1,20 @@
+/*
+	TASK    : HW1_01
+	LANG    : C++
+	AUTHOR  : Jettawat Buaruen
+	Student : 59160645
+	MAJOR   : SE
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	int sum=0;
+	cin  >> a;
+	cin  >> b;
+	sum = a+b;
+	cout << "" << sum << endl; 
+	return 0;
+}//End Main
